feef72e8b48d2b393be4d43746fdcf25f9b45577
Michael Williamson <williamson.michaelr@gmail.com>
2015-09-21 21:38:34 -0600

    Added iterative solution

--- src/perms-binary-question-marks.py
+++ src/perms-binary-question-marks.py
@@ -3,8 +3,9 @@ example_one = '1??'
 example_two = '1011?11??1?'
 example_three = '???'
 
-def compute_perms(in_string):
+example_one = '101???'
 
+def compute_perms(in_string):
   index = in_string.find('?')
   if index < 0:
     return [in_string]
@@ -12,6 +13,22 @@ def compute_perms(in_string):
   branch1 = [(in_string[:index] + '0' + '{0}').format(i) for i in compute_perms(in_string[index+1:])]
   branch2 = [(in_string[:index] + '1' + '{0}').format(i) for i in compute_perms(in_string[index+1:])]
   return branch1 + branch2
+
+def compute_perms_iterative(in_string):
+  result_set = []
+  result_set.append('')
+  for index,c in enumerate(in_string):
+    if c == '?':
+      result_set *= 2
+      for i in range(len(result_set)):
+        if i < len(result_set) / 2:
+          result_set[i] += '0'
+        else:
+          result_set[i] += '1'
+    else:
+      for i in range(len(result_set)):
+        result_set[i] += c
+
 
 def check_result(results, input_str):
   print results
@@ -22,6 +39,7 @@ check_result(compute_perms(example_one), example_one)
 check_result(compute_perms(example_two), example_two)
 check_result(compute_perms(example_three), example_three)
 
+compute_perms_iterative(example_one)
 print 'W00t We Passed!'
 
 
